86032b925d3e34526545e931b633fa64b10d4580
Milbor <not.milbor@gmail.com>
2017-06-28 16:39:55 -0400

    Renames (hopefully)

--- bullshit/__main__.py
+++ bullshit/__main__.py
@@ -4,7 +4,7 @@ from UI import *
 
 def main(argv):
     # with Database(username='DB_USER_1', password='ORACLE', host='192.168.56.101', port=1521, listener='orcl') as db:
-    db = Database(schema='DB_USER_1', username='SYSTEM', password='oracle', host='192.168.56.101', port=1521, listener='orcl')
+    db = Database(schema='DB_USER_1', username='DB_USER_1', password='oracle', host='127.0.0.1', port=1521, listener='orcl')
 
     architect = Architect(db)
     sys.exit(architect.run())
